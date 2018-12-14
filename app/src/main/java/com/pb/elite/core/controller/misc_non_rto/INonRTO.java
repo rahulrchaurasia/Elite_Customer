46f99f550e4560254d86b9ffa9de430330358bd3
nileshbirhade <nileshbirhade@gmail.com>
2018-12-14 16:16:51 +0530

    service added upto 6

--- app/src/main/java/com/pb/elite/core/controller/misc_non_rto/INonRTO.java
+++ app/src/main/java/com/pb/elite/core/controller/misc_non_rto/INonRTO.java
@@ -1,10 +1,13 @@
 package com.pb.elite.core.controller.misc_non_rto;
 
 import com.pb.elite.core.IResponseSubcriber;
+import com.pb.elite.core.requestmodel.AnalysisCurrentHealthRequestEntity;
 import com.pb.elite.core.requestmodel.ClaimGuidanceHospRequestEntity;
+import com.pb.elite.core.requestmodel.LifeInsurancePolicyNomineeRequestEntity;
 import com.pb.elite.core.requestmodel.MiscReminderPUCRequestEntity;
 import com.pb.elite.core.requestmodel.ProductPriceRequestEntity;
 import com.pb.elite.core.requestmodel.ProvideClaimAssRequestEntity;
+import com.pb.elite.core.requestmodel.SpecialBenefitsRequestEntity;
 
 /**
  * Created by Nilesh Birhade on 14-12-2018.
@@ -17,6 +20,12 @@ public interface INonRTO {
     void saveClaimGuidanceHosp(ClaimGuidanceHospRequestEntity entity, IResponseSubcriber iResponseSubcriber);
 
     void saveMiscRemiderPUC(MiscReminderPUCRequestEntity entity, IResponseSubcriber iResponseSubcriber);
+
+    void saveSpecialBenifits(SpecialBenefitsRequestEntity entity, IResponseSubcriber iResponseSubcriber);
+
+    void saveAnalysisCurrentHealth(AnalysisCurrentHealthRequestEntity entity, IResponseSubcriber iResponseSubcriber);
+
+    void saveLifeInsurancePolicyNominee(LifeInsurancePolicyNomineeRequestEntity entity, IResponseSubcriber iResponseSubcriber);
 
     void getProductTAT(ProductPriceRequestEntity entity, IResponseSubcriber iResponseSubcriber);
 }
--- app/src/main/java/com/pb/elite/core/controller/misc_non_rto/MiscNonRTOController.java
+++ app/src/main/java/com/pb/elite/core/controller/misc_non_rto/MiscNonRTOController.java
@@ -3,41 +3,22 @@ package com.pb.elite.core.controller.misc_non_rto;
 import android.content.Context;
 
 import com.pb.elite.core.IResponseSubcriber;
-import com.pb.elite.core.controller.product.AsyncCityMaster;
-import com.pb.elite.core.controller.product.IProduct;
-import com.pb.elite.core.controller.product.SyncRTOMaster;
 import com.pb.elite.core.requestbuilder.MiscNonRtoRequestBuilder;
-import com.pb.elite.core.requestbuilder.ProductRequestBuilder;
+import com.pb.elite.core.requestmodel.AnalysisCurrentHealthRequestEntity;
 import com.pb.elite.core.requestmodel.ClaimGuidanceHospRequestEntity;
-import com.pb.elite.core.requestmodel.InsertOrderRequestEntity;
+import com.pb.elite.core.requestmodel.LifeInsurancePolicyNomineeRequestEntity;
 import com.pb.elite.core.requestmodel.MiscReminderPUCRequestEntity;
 import com.pb.elite.core.requestmodel.ProductPriceRequestEntity;
 import com.pb.elite.core.requestmodel.ProvideClaimAssRequestEntity;
-import com.pb.elite.core.requestmodel.UpdateOrderRequestEntity;
-import com.pb.elite.core.response.CityResponse;
-import com.pb.elite.core.response.ClientCommonResponse;
-import com.pb.elite.core.response.DocumentResponse;
-import com.pb.elite.core.response.DocumentViewResponse;
-import com.pb.elite.core.response.NonRtoProductDisplayResponse;
-import com.pb.elite.core.response.NotificationResponse;
-import com.pb.elite.core.response.OrderDetailResponse;
-import com.pb.elite.core.response.OrderResponse;
-import com.pb.elite.core.response.ProductDocumentResponse;
+import com.pb.elite.core.requestmodel.SpecialBenefitsRequestEntity;
 import com.pb.elite.core.response.ProductPriceResponse;
-import com.pb.elite.core.response.ProductResponse;
 import com.pb.elite.core.response.ProvideClaimAssResponse;
-import com.pb.elite.core.response.RtoLocationReponse;
-import com.pb.elite.core.response.RtoProductDisplayResponse;
-import com.pb.elite.core.response.ServiceListResponse;
-import com.pb.elite.database.DataBaseController;
 import com.pb.elite.splash.PrefManager;
 
 import java.net.ConnectException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
-import java.util.HashMap;
 
-import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -57,6 +38,122 @@ public class MiscNonRTOController implements INonRTO {
         miscNonRtoNetworkService = new MiscNonRtoRequestBuilder().getService();
         mContext = context;
         prefManager = new PrefManager(mContext);
+    }
+
+    @Override
+    public void saveLifeInsurancePolicyNominee(LifeInsurancePolicyNomineeRequestEntity entity, final IResponseSubcriber iResponseSubcriber) {
+
+        miscNonRtoNetworkService.saveLifeInsurancePolicyNominee(entity).enqueue(new Callback<ProvideClaimAssResponse>() {
+            @Override
+            public void onResponse(Call<ProvideClaimAssResponse> call, Response<ProvideClaimAssResponse> response) {
+
+                if (response.isSuccessful()) {
+
+                    if (response.body().getStatus_code() == 0) {
+                        iResponseSubcriber.OnSuccess(response.body(), response.body().getMessage());
+                    } else {
+                        iResponseSubcriber.OnFailure(new RuntimeException(response.body().getMessage()));
+                    }
+
+                } else {
+                    iResponseSubcriber.OnFailure(new RuntimeException(""));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ProvideClaimAssResponse> call, Throwable t) {
+
+                if (t instanceof ConnectException) {
+                    iResponseSubcriber.OnFailure(t);
+                } else if (t instanceof SocketTimeoutException) {
+                    iResponseSubcriber.OnFailure(new RuntimeException("Check your internet connection"));
+                } else if (t instanceof UnknownHostException) {
+                    iResponseSubcriber.OnFailure(new RuntimeException("Check your internet connection"));
+                } else if (t instanceof NumberFormatException) {
+                    iResponseSubcriber.OnFailure(new RuntimeException("Unexpected server response"));
+                } else {
+                    iResponseSubcriber.OnFailure(new RuntimeException(t.getMessage()));
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void saveAnalysisCurrentHealth(AnalysisCurrentHealthRequestEntity entity, final IResponseSubcriber iResponseSubcriber) {
+        miscNonRtoNetworkService.saveAnalysisCurrentHealth(entity).enqueue(new Callback<ProvideClaimAssResponse>() {
+            @Override
+            public void onResponse(Call<ProvideClaimAssResponse> call, Response<ProvideClaimAssResponse> response) {
+
+                if (response.isSuccessful()) {
+
+                    if (response.body().getStatus_code() == 0) {
+                        iResponseSubcriber.OnSuccess(response.body(), response.body().getMessage());
+                    } else {
+                        iResponseSubcriber.OnFailure(new RuntimeException(response.body().getMessage()));
+                    }
+
+                } else {
+                    iResponseSubcriber.OnFailure(new RuntimeException(""));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ProvideClaimAssResponse> call, Throwable t) {
+
+                if (t instanceof ConnectException) {
+                    iResponseSubcriber.OnFailure(t);
+                } else if (t instanceof SocketTimeoutException) {
+                    iResponseSubcriber.OnFailure(new RuntimeException("Check your internet connection"));
+                } else if (t instanceof UnknownHostException) {
+                    iResponseSubcriber.OnFailure(new RuntimeException("Check your internet connection"));
+                } else if (t instanceof NumberFormatException) {
+                    iResponseSubcriber.OnFailure(new RuntimeException("Unexpected server response"));
+                } else {
+                    iResponseSubcriber.OnFailure(new RuntimeException(t.getMessage()));
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void saveSpecialBenifits(SpecialBenefitsRequestEntity entity, final IResponseSubcriber iResponseSubcriber) {
+
+        miscNonRtoNetworkService.saveSpecialBenifits(entity).enqueue(new Callback<ProvideClaimAssResponse>() {
+            @Override
+            public void onResponse(Call<ProvideClaimAssResponse> call, Response<ProvideClaimAssResponse> response) {
+
+                if (response.isSuccessful()) {
+
+                    if (response.body().getStatus_code() == 0) {
+                        iResponseSubcriber.OnSuccess(response.body(), response.body().getMessage());
+                    } else {
+                        iResponseSubcriber.OnFailure(new RuntimeException(response.body().getMessage()));
+                    }
+
+                } else {
+                    iResponseSubcriber.OnFailure(new RuntimeException(""));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ProvideClaimAssResponse> call, Throwable t) {
+
+                if (t instanceof ConnectException) {
+                    iResponseSubcriber.OnFailure(t);
+                } else if (t instanceof SocketTimeoutException) {
+                    iResponseSubcriber.OnFailure(new RuntimeException("Check your internet connection"));
+                } else if (t instanceof UnknownHostException) {
+                    iResponseSubcriber.OnFailure(new RuntimeException("Check your internet connection"));
+                } else if (t instanceof NumberFormatException) {
+                    iResponseSubcriber.OnFailure(new RuntimeException("Unexpected server response"));
+                } else {
+                    iResponseSubcriber.OnFailure(new RuntimeException(t.getMessage()));
+                }
+
+            }
+        });
     }
 
     @Override
